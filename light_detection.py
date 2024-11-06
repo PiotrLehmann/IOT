8cd3fbb487e14536d125347ae124bd39b9497bab
mzycho <72975565+mzycho@users.noreply.github.com>
2024-11-06 13:10:56 +0100

    Update light_detection.py

--- light_detection.py
+++ light_detection.py
@@ -1,1 +1,14 @@
 # Here will be uploaded script for printing light colour and values of its strengh from detector on raspberryPI
+import board
+import busio
+import adafruit_apds9960.apds9960
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
+
+sensor.enable_color = True
+
+while True:
+        r, g, b, c = sensor.color_data
+        print('Red: {0}, Green: {1}, Blue: {2}, Clear: {3}'.format(r, g, b, c))
+        #print*(sensor.proximity())
